4fd042c7f28bfd73a916a864fba5ec08cbb9fa37
hpellis <harriet.p.ellis@gmail.com>
2019-02-11 16:29:22 +0000

    initial commit

---
+++ dice_game.py
@@ -0,0 +1,19 @@
+from random import randint
+
+def dice_game():
+    print("Guess whether the sum of two dice will be even or odd.")
+    guess=""
+    while guess != "3":
+        guess=input("1 for even, 2 for odd or 3 to exit.")
+        dice_one=randint(1,6)
+        dice_two=randint(1,6)
+        print(dice_one + dice_two)
+        if guess == "1" and (dice_one + dice_two) % 2 == 0:
+            print ("Correct")
+        elif guess == "2" and (dice_one + dice_two) % 2 != 0:
+            print("Correct")
+        else:
+            print("Wrong")
+    print("Game over.")
+    
+dice_game()
